a79c712ab5341ceb0bc548d4ddeee221b5047a0b
Brady Kurtz <bradykurtz@churchofjesuschrist.org>
2020-08-16 14:34:26 -0600

    Removing unneeded swagger

--- src/main/java/com/weber/cms/contact/model/Contact.java
+++ src/main/java/com/weber/cms/contact/model/Contact.java
@@ -1,24 +1,7 @@
 package com.weber.cms.contact.model;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-@ApiModel(
-    value = "Api Model Value",
-    description = "Api Model Description",
-    parent = Object.class,
-    discriminator = "Api Discriminator",
-    subTypes = { Contact.class },
-    reference = "Api Reference"
-)
 public class Contact {
 
-    @ApiModelProperty(value="notFirstName",
-        allowableValues = "Allow Values",
-        access = "Read Write",
-        notes = "My note",
-        dataType = "Number",
-        required = true,
-        hidden = false)
     private String firstName;
 
     private String lastName;
