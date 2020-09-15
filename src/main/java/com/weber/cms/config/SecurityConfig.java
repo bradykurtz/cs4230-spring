c5a486f6c20c2152d6728704ffc8728b3ffe56fa
Brady Kurtz <bradykurtz@churchofjesuschrist.org>
2020-09-15 16:02:35 -0600

    checking in security stuff

--- src/main/java/com/weber/cms/config/SecurityConfig.java
+++ src/main/java/com/weber/cms/config/SecurityConfig.java
@@ -2,13 +2,26 @@ package com.weber.cms.config;
 
 import com.weber.cms.user.UserService;
 import com.weber.cms.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public SecurityConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
 
     @Bean
     public UserRepository userRepository(NamedParameterJdbcTemplate jdbcTemplate) {
@@ -20,4 +33,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new UserService(userRepository);
     }
 
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return applicationContext.getBean(UserService.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
